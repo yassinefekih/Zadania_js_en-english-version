26877bdd5bbb911cd1dc2985386e9904102f06b2
yassine fekih <yfekihsaid@sevillemore.com>
2021-06-24 21:35:00 +0100

    step 6 Ok

--- 5. Functions of time/script.js
+++ 5. Functions of time/script.js
@@ -2,3 +2,19 @@
 var n =1;
 let timerId = setInterval(() => {console.log(n+": hello again");n++;}, 3000);
 setTimeout(() => { clearInterval(timerId) }, 15000);
+//////
+var number =1;
+var table =["1","2","3","red","blue","green"];
+setTimeout(() => {
+    table.map((element)=>console.log(element));
+    setTimeout(()=>
+        {
+            setInterval(() => {
+                        table.map((element)=>console.log(element));
+                        table.push(number+"more");
+                        number++;
+                    }, 3000);}
+                ,3000);
+     }, 2000);
+
+
